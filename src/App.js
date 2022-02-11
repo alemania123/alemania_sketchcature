ba8462cef35a0038ec02c38fbb99fe5a85117b29
Camille Alemania <calemania_180000000545@uic.edu.ph>
2022-02-12 07:52:59 +0800

    Added css and javascript files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ViewItems from "./pages/ViewItems";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Technology from "./pages/Technology";
+import Sale from "./pages/Sale";
+import Contact from "./pages/Contacts";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/viewitems" element={<ViewItems />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/sale" element={<Sale />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
