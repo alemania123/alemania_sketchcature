80d2dbe6b1e15f33c9e87de76c5a5cf63942ec6f
Camille Alemania <calemania_180000000545@uic.edu.ph>
2022-02-12 07:46:23 +0800

    Added web pages

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Banner from "../assets/Banner.jpg";
+function Home() {
+  return (
+    <div>
+      <img src={Banner} className="" alt="" />
+    </div>
+  );
+}
+
+export default Home;
